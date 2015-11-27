0187cd3f4ffb0fed3411e784e0012382f1c3c9f5
Jyotsna Prakash <jyotsna.prakash@gmail.com>
2015-11-27 09:17:12 -0800

    comment moe markdown titles resizing
    I have such a hard time deleting lines...

--- init.el
+++ init.el
@@ -65,9 +65,9 @@
 ;;(load-theme 'moe-light t)
 (require 'moe-theme)
 ;; Resize titles (optional).
-(setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0))
-(setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
-(setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
+;;(setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0))
+;;(setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
+;;(setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
 (moe-theme-set-color 'cyan)
 (setq moe-theme-highlight-buffer-id nil)
 ;; dark/light depending on time of day see Readme
