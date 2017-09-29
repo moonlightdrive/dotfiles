a325a75747c29384adc95f39a085c873cb9e3301
Jyotsna Prakash <jyotsna.prakash@gmail.com>
2017-09-29 08:37:21 -0700

    [xmonad] modify keybindings for bsp

--- xmonad.hs
+++ xmonad.hs
@@ -41,19 +41,19 @@ myManageHook = composeAll
 
 myKeys =
        [ ("M-p", spawn "rofi -show run")
-       --	 , ("M-M1-<Left>",    sendMessage $ ExpandTowards L)
+
        , ("M-M1-h", sendMessage $ ExpandTowards L)
-       --	 , ("M-M1-<Right>",   sendMessage $ ShrinkFrom L)
        , ("M-M1-k", sendMessage $ ExpandTowards U)
-  --	 , ("M-M1-<Down>",    sendMessage $ ShrinkFrom U)
-  --	 , ("M-M1-C-<Left>",  sendMessage $ ShrinkFrom R)
-  --	 , ("M-M1-C-<Right>", sendMessage $ ExpandTowards R)
        , ("M-M1-l", sendMessage $ ExpandTowards R)
-  --	 , ("M-M1-C-<Up>",    sendMessage $ ShrinkFrom D)
-  --	 , ("M-M1-C-<Down>",  sendMessage $ ExpandTowards D)
-         , ("M-M1-j", sendMessage $ ExpandTowards D)
-	 , ("M-s", sendMessage $ Swap)
-         , ("M-r", sendMessage $ Rotate)
+       , ("M-M1-j", sendMessage $ ExpandTowards D)
+       -- do i really need these
+       , ("M-S-M1-h", sendMessage $ ShrinkFrom L)
+       , ("M-S-M1-k", sendMessage $ ShrinkFrom U)
+       , ("M-S-M1-l", sendMessage $ ShrinkFrom R)
+       , ("M-S-M1-j", sendMessage $ ShrinkFrom D)
+
+       , ("M-s", sendMessage $ Swap)
+       , ("M-r", sendMessage $ Rotate)
        ]
 
 prettyPrinter :: D.Client -> PP
